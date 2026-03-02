5810056a958b95f49e0b149bbc0c8df8afdd2ac0
nilsachy <nils.achy@gmail.com>
2026-03-02 09:19:47 +0000

    feat(visibility): add custom port option for visibility server

--- cmd/kueue/main.go
+++ cmd/kueue/main.go
@@ -318,7 +318,7 @@ func main() {
 
 	if features.Enabled(features.VisibilityOnDemand) {
 		go func() {
-			if err := visibility.CreateAndStartVisibilityServer(ctx, queues, *cfg.InternalCertManagement.Enable, kubeConfig); err != nil {
+			if err := visibility.CreateAndStartVisibilityServer(ctx, queues, *cfg.InternalCertManagement.Enable, kubeConfig, visibilityServerPort); err != nil {
 				setupLog.Error(err, "Unable to create and start visibility server")
 				os.Exit(1)
 			}
