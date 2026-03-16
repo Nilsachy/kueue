4ab1aed09d6a1b4f35cff2133b48daaee2734ba6
nilsachy <nils.achy@gmail.com>
2026-03-16 13:03:26 +0000

    feat: rename `--visibility-secure-port` flag to `--visibility-server-port` in documentation, tests, and main command.

--- cmd/kueue/main.go
+++ cmd/kueue/main.go
@@ -129,8 +129,8 @@ func main() {
 	var featureGates string
 	flag.StringVar(&featureGates, "feature-gates", "", "A set of key=value pairs that describe feature gates for alpha/experimental features.")
 
-	var visibilitySecurePort int
-	flag.IntVar(&visibilitySecurePort, "visibility-secure-port", 8082, "The port the visibility server binds to.")
+	var visibilityServerPort int
+	flag.IntVar(&visibilityServerPort, "visibility-server-port", 8082, "The port the visibility server binds to.")
 
 	customLogProcessor := zaplog.WrapCore(utillogging.NewCustomLogProcessor)
 
@@ -363,7 +363,7 @@ func main() {
 
 	if features.Enabled(features.VisibilityOnDemand) {
 		go func() {
-			if err := visibility.CreateAndStartVisibilityServer(ctx, queues, *cfg.InternalCertManagement.Enable, kubeConfig, visibilitySecurePort, parsedTLSConfig); err != nil {
+			if err := visibility.CreateAndStartVisibilityServer(ctx, queues, *cfg.InternalCertManagement.Enable, kubeConfig, visibilityServerPort, parsedTLSConfig); err != nil {
 				setupLog.Error(err, "Unable to create and start visibility server")
 				os.Exit(1)
 			}
