307aeb4de4cc6f578882f9c4b8d8f78241a8395d
nilsachy <nils.achy@gmail.com>
2026-03-20 15:10:47 +0000

    Revert "Deprecate the visibility-server-port flag"
    This reverts commit 7c619ba5092985f77540a7e9b762cca4c86280db.

--- test/e2e/customconfigs/visibility_server_test.go
+++ test/e2e/customconfigs/visibility_server_test.go
@@ -18,6 +18,7 @@ package customconfigse2e
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/onsi/ginkgo/v2"
 	"github.com/onsi/gomega"
@@ -212,6 +213,36 @@ var _ = ginkgo.Describe("Visibility Server", func() {
 		}
 		util.MustCreate(ctx, k8sClient, authDelegatorBinding)
 
+		gomega.Eventually(func(g gomega.Gomega) {
+			visClient := util.CreateVisibilityClient("")
+			pw, err := visClient.ClusterQueues().GetPendingWorkloadsSummary(ctx, cqName, metav1.GetOptions{})
+			g.Expect(err).NotTo(gomega.HaveOccurred())
+			g.Expect(pw).NotTo(gomega.BeNil())
+		}, util.Timeout, util.Interval).Should(gomega.Succeed())
+	})
+
+	ginkgo.It("Should use the custom port from the --visibility-server-port flag", func() {
+		ginkgo.By("Updating the visibility-server service's targetPort")
+		patchedService := originalService.DeepCopy()
+		for i, p := range patchedService.Spec.Ports {
+			if p.Name == "https" {
+				patchedService.Spec.Ports[i].TargetPort = intstr.FromInt32(customVisibilityPort)
+			}
+		}
+		gomega.Expect(k8sClient.Update(ctx, patchedService)).To(gomega.Succeed())
+
+		ginkgo.By("Updating the visibility-server deployment's port")
+		patchedDeployment := originalDeployment.DeepCopy()
+		for i, c := range patchedDeployment.Spec.Template.Spec.Containers {
+			if c.Name == "manager" {
+				container := &patchedDeployment.Spec.Template.Spec.Containers[i]
+				container.Args = append(c.Args, fmt.Sprintf("--visibility-server-port=%d", customVisibilityPort))
+			}
+		}
+		gomega.Expect(k8sClient.Update(ctx, patchedDeployment)).To(gomega.Succeed())
+		util.WaitForKueueAvailabilityNoRestartCountCheck(ctx, k8sClient)
+
+		ginkgo.By("Verifying requests succeed on the custom port")
 		gomega.Eventually(func(g gomega.Gomega) {
 			visClient := util.CreateVisibilityClient("")
 			pw, err := visClient.ClusterQueues().GetPendingWorkloadsSummary(ctx, cqName, metav1.GetOptions{})
