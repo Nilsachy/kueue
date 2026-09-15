e194fca8069bb4867ace694091ae2152ed53d62d
Vlad <logicvsua@gmail.com>
2026-09-15 10:37:10 +0200

    Join configurable preemption results with existing algorithms
    Add configurable preemption results to existing.

--- pkg/scheduler/preemption/preemption.go
+++ pkg/scheduler/preemption/preemption.go
@@ -155,17 +155,31 @@ func (p *Preemptor) GetTargets(ctx context.Context, wl workload.Info, assignment
 }
 
 func (p *Preemptor) getTargets(preemptionCtx *preemptionCtx) []*Target {
-	// Configurable preemptions handled first to allow smooth fallback
-	// to previous algorithms when the feature is disabled.
+	var targets []*Target
+	if p.enableFairSharing {
+		targets = p.fairPreemptions(preemptionCtx, p.fsStrategies)
+	} else {
+		targets = p.classicalPreemptions(preemptionCtx)
+	}
+
 	if features.Enabled(features.ConfigurablePreemption) {
 		if preemptionCtx.preemptorCQ.PreemptionConfigName != nil {
-			return p.configurablePreemptions(preemptionCtx)
+			targetsSet := sets.New[workload.Reference]()
+			for _, target := range targets {
+				targetsSet.Insert(workload.Key(target.WorkloadInfo.Obj))
+			}
+			configurableTargets := p.configurablePreemptions(preemptionCtx)
+			for _, configurableTarget := range configurableTargets {
+				key := workload.Key(configurableTarget.WorkloadInfo.Obj)
+				if !targetsSet.Has(key) {
+					targetsSet.Insert(key)
+					targets = append(targets, configurableTarget)
+				}
+			}
 		}
 	}
-	if p.enableFairSharing {
-		return p.fairPreemptions(preemptionCtx, p.fsStrategies)
-	}
-	return p.classicalPreemptions(preemptionCtx)
+
+	return targets
 }
 
 var HumanReadablePreemptionReasons = map[string]string{
