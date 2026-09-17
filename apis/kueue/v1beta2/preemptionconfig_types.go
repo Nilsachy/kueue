2b2fa76b111fd61bc22e65d6d9b8976756d9075c
Danylo Tiutiushkin <48014331+danylott@users.noreply.github.com>
2026-09-17 11:31:40 +0200

    Preemption Config Label Candidate Selector implementation (#29)
    * Implement LabelSelector workload filter candidate selector for Preemption Config
    
    * Deleted unnecessary file
    
    * change log level & add empty line

--- apis/kueue/v1beta2/preemptionconfig_types.go
+++ apis/kueue/v1beta2/preemptionconfig_types.go
@@ -168,6 +168,11 @@ type PreemptionCandidateSelector struct {
 	// +optional
 	NumericLabels []NumericLabelConstraint `json:"numericLabels,omitempty"`
 
+	// LabelSelector defines label selector constraints on candidate Workloads.
+	// Accepts all if not set.
+	// +optional
+	LabelSelector *metav1.LabelSelector `json:"labelSelector,omitempty"`
+
 	// RelativeWorkloadPriority defines how the preemptor's priority compares to the candidate's priority.
 	// For example "Lower" means that only workloads with lower priority will be allowed as preemption candidates.
 	// The comparison is made using effective priority (accounting for priority boost if enabled).
