54377891c3880ccdc7dad56af1f84d0188cabcfc
nilsachy <nilsachy@google.com>
2026-09-22 18:16:54 +0000

    replace preemptioncommon constant with literal string in configurable preemption e2e test

--- test/e2e/sequential/baseline/configurable_preemptions_test.go
+++ test/e2e/sequential/baseline/configurable_preemptions_test.go
@@ -30,7 +30,6 @@ import (
 	kueue "sigs.k8s.io/kueue/apis/kueue/v1beta2"
 	workloadjob "sigs.k8s.io/kueue/pkg/controller/jobs/job"
 	"sigs.k8s.io/kueue/pkg/features"
-	preemptioncommon "sigs.k8s.io/kueue/pkg/scheduler/preemption/common"
 	utiltestingapi "sigs.k8s.io/kueue/pkg/util/testing/v1beta2"
 	jobtesting "sigs.k8s.io/kueue/pkg/util/testingjobs/job"
 	"sigs.k8s.io/kueue/test/util"
@@ -168,7 +167,7 @@ var _ = ginkgo.Describe("Configuration Preemptions", ginkgo.Label("feature:confi
 					metav1.Condition{
 						Type:   kueue.WorkloadPreempted,
 						Status: metav1.ConditionTrue,
-						Reason: preemptioncommon.ConfigurablePreemptionReason,
+						Reason: "ConfigurablePreemption",
 					},
 					cmpopts.IgnoreFields(metav1.Condition{}, "LastTransitionTime", "Message", "ObservedGeneration"),
 				)))
